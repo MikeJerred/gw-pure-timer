fbb9915636e55be788ab80b1cb7bd015ffe6a7d2
Michael Jerred <mjerred.work@gmail.com>
2021-02-14 21:53:22 +0000

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval, Observable, Subject } from 'rxjs';
+import { map, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,103 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pure-timer';
+  alcoholTimer: number | null = null;
+  eggTimer: number | null = null;
+  allTimer: number | null = null;
+
+  readonly buffer = 5000;
+  private readonly tickLength = 100;
+  private readonly alcoholRefreshTime = 2 * 60 * 1000;
+  private readonly eggRefreshTime = 5 * 60 * 1000;
+  private readonly allRefreshTime = 10 * 60 * 1000;
+
+  private stopAlcohol$ = new Subject<void>();
+  private stopEgg$ = new Subject<void>();
+  private stopAll$ = new Subject<void>();
+
+  isDanger(time: number | null) {
+    return time !== null && time <= this.buffer + 1000;
+  }
+
+  startAlcohol() {
+    this.stopAlcohol$.next();
+    this.alcoholTimer = this.alcoholRefreshTime;
+
+    this.createTimer(this.stopAlcohol$, this.alcoholRefreshTime, 'alkahol').subscribe(timeLeft => {
+      this.alcoholTimer = timeLeft;
+
+      if (timeLeft <= 0) {
+        this.startAlcohol();
+      }
+    });
+  }
+
+  stopAlcohol() {
+    this.stopAlcohol$.next();
+    this.alcoholTimer = null;
+  }
+
+  startEgg() {
+    this.stopEgg$.next();
+    this.eggTimer = this.eggRefreshTime;
+
+    this.createTimer(this.stopEgg$, this.eggRefreshTime, 'Egg').subscribe(timeLeft => {
+      this.eggTimer = timeLeft;
+
+      if (timeLeft <= 0) {
+        this.startEgg();
+      }
+    });
+  }
+
+  stopEgg() {
+    this.stopEgg$.next();
+    this.eggTimer = null;
+  }
+
+  startAll() {
+    this.stopAll$.next();
+    this.allTimer = this.allRefreshTime;
+
+    this.createTimer(this.stopAll$, this.allRefreshTime, 'Corn').subscribe(timeLeft => {
+      this.allTimer = timeLeft;
+
+      if (timeLeft <= 0) {
+        this.startAll();
+      }
+    });
+  }
+
+  stopAll() {
+    this.stopAll$.next();
+    this.allTimer = null;
+  }
+
+  private createTimer(stop$: Observable<void>, refreshTime: number, text: string) {
+    let state = 100;
+
+    return interval(this.tickLength).pipe(
+      map(ticks => refreshTime - ticks * this.tickLength),
+      tap(timeLeft => {
+        if (timeLeft < this.buffer + 1000) {
+          const currentState = Math.floor(timeLeft / 1000);
+          if (state > currentState) {
+            const speechText =
+              state === 100 ? text
+              : currentState > 0 ? `${currentState}`
+              : 'pop ' + text;
+
+            state = currentState;
+
+
+            const utterance = new SpeechSynthesisUtterance(speechText);
+            utterance.rate = 2;
+            window.speechSynthesis.cancel();
+            window.speechSynthesis.speak(utterance);
+          }
+        }
+      }),
+      takeUntil(stop$)
+    );
+  }
 }
---
+++ src/app/timer.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'timer' })
+export class TimerPipe implements PipeTransform {
+  transform(value: number | null): string {
+    if (value === null) return '-';
+
+    const seconds = Math.floor((value / 1000) % 60);
+    const minutes = Math.floor(value / 60000);
+
+    return `${minutes}:${seconds.toPrecision()}`;
+  }
+}
